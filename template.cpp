b2ea43c7fb1437826cc95747744db3e3f347568f
VICTOR VIEIRA <victorbor2001@yahoo.com.br>
2022-07-15 01:25:09 -0300

    Updated template

--- template.cpp
+++ template.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 
 typedef long long int lli;
+typedef unsigned long long int ulli;
 #define ibs ios_base::sync_with_stdio(false);
 #define cti cin.tie(NULL);
 #define endl "\n"
